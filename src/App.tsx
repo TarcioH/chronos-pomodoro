000f534606f07f24805efcae19f86ca6d62a526b
Tárcio Rodrigues <tarcio.dev@gmail.com>
2025-05-24 16:39:14 -0300

    criando as camadas do crnos

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+
 import './styles/themes.css';
 import './styles/global.css';
-import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
+      </Container>
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
